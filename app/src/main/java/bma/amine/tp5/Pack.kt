32d050d83159dfc342054507f5ff29b2a1bbeb31
Amine BENBAKHTA <gm_benbakhta@esi.dz>
2020-04-17 01:57:32 +0100

    Deleting some unused code/activity

--- app/src/main/java/bma/amine/tp5/Pack.kt
+++ app/src/main/java/bma/amine/tp5/Pack.kt
@@ -6,6 +6,4 @@ class Pack(val giftName:String, val giftQte:Int, name: String, price: Long, Qte:
     var smartphoneList:MutableMap<Smartphone,Int> = mutableMapOf()
 
     fun addSmartphone(s:Smartphone,qtePhone:Int) = smartphoneList.put(s,qtePhone)
-
-    fun getType():String = "Pack"
 }
